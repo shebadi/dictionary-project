578786e1b830deaf976b240a345a82f146e981bc
ShabnamEbadi <sh.ebady@yahoo.com>
2022-04-09 13:12:36 +0430

    Added initial searching(dictionary component)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="continer">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Shabnam Ebadi</footer>
+      </div>
     </div>
   );
 }
